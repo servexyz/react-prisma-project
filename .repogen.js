541ac996c3430f3f0d487fe5135666738677f054
alechp <me@alechp.com>
2018-06-12 11:35:46 -0700

    Initialized repogen

---
+++ .repogen.js
@@ -0,0 +1,10 @@
+const config = {
+  provider: "alechp",
+  repospacePath: __dirname,
+  repositories: [
+    { servexyz: "react-project" },
+    { servexyz: "prisma-project" },
+  ]
+};
+
+module.exports = { config };
